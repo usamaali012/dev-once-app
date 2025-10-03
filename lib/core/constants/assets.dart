8190f85a6f97ec021aa35f0e1f338aec3fcf2957
Usama Ali <usamaali012@gmail.com>
2025-10-03 12:50:17 +0500

    SVGs added

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,17 @@
+/// Centralized asset and font references.
+class AppAssets {
+  // Base paths
+  static const String svgs = 'assets/svgs';
+  static const String fonts = 'assets/fonts';
+
+  // Auth SVGs
+  static const String authDo = '$svgs/auth-do.svg';
+  static const String authRoundedShapesVertical = '$svgs/auth-round-shapes-vertical.svg';
+  static const String mobileIcon = '$svgs/mobile-icon.svg';
+  static const String user = '$svgs/user.svg';
+  static const String lock = '$svgs/lock.svg';
+}
+
+class AppFonts {
+  static const String poppins = 'Poppins';
+}
