4b4d6b8a97a06508e58189b72023817646e559b8
Usama Ali <usamaali012@gmail.com>
2025-10-09 12:34:17 +0500

    VM set

--- lib/features/caution/caution_vm.dart
+++ lib/features/caution/caution_vm.dart
@@ -10,6 +10,7 @@ class CautionVm extends BaseProvider {
   // 0 = Father, 1 = Husband
   int guardianType = 0;
 
+  MandatoryInfo data = MandatoryInfo();
   String? fatherName;
   String? husbandName;
   String? mobile;
@@ -67,18 +68,22 @@ class CautionVm extends BaseProvider {
   String? onRelationValidate(String? v) => v == null || v.isEmpty ? 'Relation is required' : null;
 
   Future<({bool success, String? message})> get() async {
+    setLoading(true);
     final config = RequestConfig<MandatoryInfo>(
       endpoint: '/user/get-mandatory-info',
+      fromJson: MandatoryInfo.fromJson
     );
 
     final response = await client.get(config);
 
     if(response.success) {
-      MandatoryInfo mandatoryInfo = response.data!;
-      if (mandatoryInfo.husbandName.existAndNotEmpty) { guardianType = 1; }
+      data = response.data!;
+      if (data.husbandName.existAndNotEmpty) { guardianType = 1; }
       else { guardianType = 0; }
+      setLoading(false);
       return (success: true, message: null);
     } else {
+      setLoading(false);
       return (success: false, message: response.message);
     }
   }
@@ -99,7 +104,7 @@ class CautionVm extends BaseProvider {
     );
 
     final config = RequestConfig(
-      endpoint: '/auth/update-mandatory-info',
+      endpoint: '/user/update-mandatory-info',
       request: request.toJson(),
     );
 
@@ -111,6 +116,12 @@ class CautionVm extends BaseProvider {
     } else {
       return (success: false, message: response.message);
     }
+  }
+
+  bool isLoading = false;
+  void setLoading(bool value) {
+    isLoading = value;
+    notifyListeners();
   }
 }
 
