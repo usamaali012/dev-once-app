bd2dad4bba3c4134ca6d854f0620957aee0093f8
Usama Ali <usamaali012@gmail.com>
2025-10-20 13:06:08 +0500

    Removed previous icons added new do icon

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -5,17 +5,9 @@ class AppAssets {
   static const String fonts = 'assets/fonts';
 
   // Auth SVGs
-  static const String authDo = '$svgs/auth-do.svg';
-  static const String authRoundedShapesVertical = '$svgs/auth-round-shapes-vertical.svg';
-  static const String mobileIcon = '$svgs/mobile-icon.svg';
+  static const String do_ = '$svgs/do.svg';
   static const String user = '$svgs/user.svg';
   static const String lock = '$svgs/lock.svg';
-  static const String dashboardTop = '$svgs/dashboard-top.svg';
-  static const String dashboardMiddle = '$svgs/dashboard-middle.svg';
-  static const String goTo = '$svgs/go-to.svg';
-  static const String transferRollover = '$svgs/transfer-rollover.svg';
-  static const String userEdit = '$svgs/user-edit.svg';
-  static const String withdraw = '$svgs/withdraw.svg';
 }
 
 class AppFonts {
